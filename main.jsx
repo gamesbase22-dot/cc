70cb7ff55ed483de27492910be271c553d01eb2a
gamesbase22-dot <games.base22@gmail.com>
2026-01-13 04:45:06 -0300

    dd

--- main.jsx
+++ main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './app.jsx'
-import './index.css'
+import App from './App.jsx'
+
+// REMOVEMOS a linha do index.css para não dar erro no Vercel
+// O estilo já é garantido pelo Tailwind no index.html
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
